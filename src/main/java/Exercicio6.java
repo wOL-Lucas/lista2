c283f216ce960bb4d13c7ac337060b2789ddaa62
wOL-Lucas <lucasefernandes333@gmail.com>
2023-09-09 16:40:35 -0300

    finished exerc7

--- src/main/java/Exercicio6.java
+++ src/main/java/Exercicio6.java
@@ -3,27 +3,76 @@ leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do j
 pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24
 horas.
 */
-
 import java.util.Scanner;
 
-public class Exercicio6 {
+import static java.lang.Math.round;
 
-    public static String[] getHoraInicial(Scanner scanner) throws Exception{
-        System.out.println("Digite a hora inicial: ");
-        try{
-            String fullDate = scanner.next();
-            String[] date = fullDate.split(":");
-            if(date.length != 2){
-                throw new Exception("Formato de hora inválido. Use o formato hora:minuto");
+
+public class Exercicio6{
+
+    public static double[] getTimes(Scanner scanner) {
+        double[] times = new double[2];
+        String[] frases = {"Digite a hora inicial: ", "Digite a hora final: "};
+        int i = 0;
+
+        while (i <= 1) {
+            System.out.println(frases[i]);
+            try {
+                String horaInicial = scanner.nextLine();
+                String[] horaInicialArray = horaInicial.split(":");
+                if (horaInicialArray.length != 2) {
+                    throw new Exception("Hora inicial deve ser no formato HH:mm");
+                } else if (Double.parseDouble(horaInicialArray[0]) < 0 || Double.parseDouble(horaInicialArray[0]) > 24 || Double.parseDouble(horaInicialArray[1]) > 59 || Double.parseDouble(horaInicialArray[1]) < 0) {
+                    throw new Exception("Hora inicial deve ser entre 0 e 24 e os minutos entre 0 e 60");
+                }
+
+                times[i] = Double.parseDouble(horaInicialArray[0]) * 60 + Double.parseDouble(horaInicialArray[1]);
+                i +=1;
+
+            } catch (Exception e) {
+                if (e instanceof java.lang.NumberFormatException) {
+                    System.out.println("Hora inicial deve ser um número");
+                    scanner.next();
+                    return getTimes(scanner);
+                } else {
+                    System.out.println("Algo deu errado: " + e);
+                    return getTimes(scanner);
+                }
             }
-
-            return date;
-
-        }catch(Exception e){
-            throw e;
-
         }
+        return times;
+    }
+
+    public static String CalculateTime(double[] times){
+        double initialTime,finalTime,minutes,totalHours;
+        initialTime = times[0] / 60;
+        finalTime = times[1] / 60;
+
+
+
+        if(finalTime == initialTime){
+            totalHours = 24;
+        } else if (finalTime < initialTime) {
+            totalHours = 24 + (finalTime - initialTime);
+        }
+        else{
+            totalHours = finalTime - initialTime;
+        }
+
+        String [] totalHoursArray = String.valueOf(totalHours).split("\\.");
+        if(Double.parseDouble(totalHoursArray[1]) > 0.01){
+            minutes = round((Double.parseDouble(totalHoursArray[1]) * 0.6 + 3) / 100) * 100;
+            return totalHoursArray[0] + ":" + String.format("%.0f", minutes).substring(0,2) + "h";
+        }
+
+        return String.format("%.2f", totalHours);
+    }
+
+    public static void main(String [] args){
+        double[] horas = getTimes(new Scanner(System.in));
+        System.out.println("A quantidade de horas jogadas e: " + CalculateTime(horas));
 
 
     }
+
 }
---
+++ src/main/java/Exercicio7.java
@@ -0,0 +1,103 @@
+/*
+Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste
+item. A seguir, calcule e mostre o valor da conta a pagar.
+
+1     cachorro quente  4.00
+2     x-salada         4.50
+3     x-bacon          5.00
+4     torrada simples  2.00
+5     refrigerante     1.50
+
+
+*/
+
+
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import org.apache.commons.lang3.tuple.Pair;
+public class Exercicio7 {
+
+    public static int getProductCode(Scanner scanner){
+        System.out.println("Digite o código do produto: ");
+        try{
+            return scanner.nextInt();
+        }
+        catch(Exception e){
+            if(e instanceof InputMismatchException){
+                System.out.println("Código deve ser um número inteiro");
+                scanner.next();
+                return getProductCode(scanner);
+            }
+            else{
+                System.out.println("Algo deu errado:" + e);
+                return getProductCode(scanner);
+            }
+        }
+    }
+
+    public static int getProductQuantity(Scanner scanner){
+        System.out.println("Digite a quantidade do produto: ");
+        try{
+            return scanner.nextInt();
+        }
+        catch(Exception e){
+            if(e instanceof InputMismatchException){
+                System.out.println("Quantidade deve ser um número inteiro");
+                scanner.next();
+                return getProductQuantity(scanner);
+            }
+            else{
+                System.out.println("Algo deu errado:" + e);
+                return getProductQuantity(scanner);
+            }
+        }
+    }
+
+    public static Pair<String,Double> getProductPrice(int codigo, HashMap<Integer, Pair<String,Double>> map){
+        try {
+            return map.get(codigo);
+        }
+        catch(Exception e){
+            if(e instanceof NullPointerException){
+                System.out.println("Código não encontrado");
+                return null;
+            }
+            else{
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+    public static void main(String [] args) throws RuntimeException{
+
+
+        HashMap<Integer, Pair<String,Double>> map = new HashMap<Integer,Pair<String,Double>>();
+        map.put(1,Pair.of("cachorro quente",4.00));
+        map.put(2,Pair.of("x-salada",4.50));
+        map.put(3,Pair.of("x-bacon",5.00));
+        map.put(4,Pair.of("torrada simples",2.00));
+        map.put(5,Pair.of("refrigerante",1.50));
+
+        Scanner scanner = new Scanner(System.in);
+        int codigo = getProductCode(scanner);
+        int quantidade = getProductQuantity(scanner);
+
+        Pair<String,Double> produto = getProductPrice(codigo, map);
+        try{
+            String nomeProduto = produto.getLeft();
+            Double preco = produto.getRight();
+            System.out.printf("Você comprou " + quantidade + " "+ nomeProduto + "\nTotal: R$ %.2f%n",(preco * Double.parseDouble(Integer.toString(quantidade))));
+        }
+        catch(Exception e){
+            if(e instanceof NullPointerException){
+                System.out.println("Produto não encontrado");
+            }
+            else{
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
